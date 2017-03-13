e9592acfaf73cc8eb4e42e4c833cad6aef4bbfab
Bjim <imbjim1@gmail.com>
2017-03-13 16:54:09 +0200

    2D array added

--- rover.js
+++ rover.js
@@ -1,9 +1,10 @@
 /*
 Develop an api that moves a rover around on a grid.
 You are given the initial starting point (x,y) of a rover and the direction (N,S,E,W) it is facing.
-The rover receives a character array of commands.
-Implement commands that move the rover forward/backward (f,b).
-Implement commands that turn the rover left/right (l,r).
+1. Create an object to represent the rover that has position and direction attributes.
+2. Create a grid using arrays.
+3. Write functions for the various commands: forward/backward (f,b), left/right (l,r).
+4. Try to call some of those functions and display the new position of the rover.
 Implement wrapping from one edge of the grid to another. (planets are spheres after all)
 Implement obstacle detection. The rover should execute the given commands until it reaches an obstacle, then stop at the last possible position and report the obstacle.
 */
@@ -20,6 +21,39 @@ var obstacle = {
     obstaclePosition : [Math.floor((Math.random() * 9)+1), Math.floor((Math.random() * 10))]
 };
 
+var map = {
+  grid: array2D(10,10,0)
+};
+
+function array2D(numrows, numcols, initial){//bidimensional array generated
+   var arr = [];
+   for (var i = 0; i < numrows; ++i){
+      var columns = [];
+      for (var x = 0; x < numcols; ++x){
+         columns[x] = "["+i+","+x+"]"; // si quisiera el valor incial solo sustituir por initial
+      }
+      arr[i] = columns;
+    }
+    return arr;
+}
+
+function mapIni(){
+  map.grid[obstacle.obstaclePosition[0]][obstacle.obstaclePosition[1]]= "Obstacle";
+  map.grid[0][0]= "Rover";
+  map.rover = '[0,0]';
+  map.roverNew= '[0,0]';
+  map.roverP0 = 0;
+  map.roverP1= 0;
+  map.roverP0New= 0;
+  map.roverP1New= 0;
+}
+function refreshGrid(){
+  map.grid[map.roverP0][map.roverP1]= map.rover;
+  map.grid[map.roverP0New][map.roverP1New]= "Rover";
+  map.rover = map.roverNew;
+  map.roverP1 = map.roverP1New;
+  map.roverP0 = map.roverP0New;
+}
 
 function verify(command) {
   var command = document.getElementById("instr").value;
@@ -59,6 +93,9 @@ function inputText() {
         }
   }
   document.getElementById("output").innerHTML = "Rover's position: X = " + myRover.position[0] + ", Y = " + myRover.position[1] + "; facing " + myRover.direction + "; status: " + myRover.status + ";<br> obstacle's position: X = " + obstacle.obstaclePosition[0] + ", Y = " + obstacle.obstaclePosition[1] + ".";
+  console.log("Final rover position: [" + myRover.position[0] + ", " + myRover.position[1] + "], Status: "+myRover.status);
+  refreshGrid();
+  console.log("Updated map:\n"+ map.grid);
   return true;
 }
   
@@ -68,19 +105,19 @@ function doMove(move) {
   var stepY = 0;
   // north
  if (myRover.direction === 'North') { 
-    stepX = 1;
+ stepX = 1;
  }
   // south
   else if (myRover.direction === 'South') { 
-          stepX = -1;
+  stepX = -1;
   }
    // east
    else if (myRover.direction === 'East') { 
-            stepY = 1;
+     stepY = 1;
    }
    // west
    else if (myRover.direction === 'West') {
-            stepY = -1;
+      stepY = -1;
    }
     // MOVING BACKWARDS
     if (move === 'b') { 
@@ -157,10 +194,13 @@ function doTurn(move){
     }
 }
 
-function thereIsAnObstacle(tempPosition) {
+function thereIsAnObstacle(newPosition) {
   if ((obstacle.obstaclePosition[0] === myRover.newPosition[0]) && (obstacle.obstaclePosition[1] === myRover.newPosition[1])){
     myRover.status = '#$@&%*! aka obstructed';
     return true;
   }
   return false;
 } 
+
+mapIni();
+console.log("Initial grid map:\n"+ map.grid);
